d8b7a352c068c12e77ac8f31e4fe087362855c11
kecron615 <katherineecron@gmail.com>
2024-03-05 14:47:50 -0600

    Week 13

---
+++ pet-store/src/main/java/pet/store/entity/Customer.java
@@ -0,0 +1,25 @@
+package pet.store.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.ToString;
+
+import java.util.Set;
+
+@Entity
+@Data
+public class Customer {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long customerId;
+    
+    private String customerEmail;
+    
+    @ManyToMany(mappedBy = "customers", cascade = CascadeType.PERSIST)
+    @EqualsAndHashCode.Exclude
+    @ToString.Exclude
+    private Set<PetStore> petStores;
+}
+
---
+++ pet-store/src/main/java/pet/store/entity/PetStore.java
@@ -0,0 +1,40 @@
+package pet.store.entity;
+
+import java.util.Set;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.OneToMany;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.ToString;
+
+@Entity
+@Data
+public class PetStore {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long petStoreId;
+    
+    private String petStoreName;
+    
+    @ManyToMany(cascade = CascadeType.PERSIST)
+    @JoinTable(name = "pet_store_customer",
+               joinColumns = @JoinColumn(name = "pet_store_id"),
+               inverseJoinColumns = @JoinColumn(name = "customer_id"))
+    @EqualsAndHashCode.Exclude
+    @ToString.Exclude
+    private Set<Customer> customers;
+    
+    @OneToMany(mappedBy = "petStore", cascade = CascadeType.ALL, orphanRemoval = true)
+    @EqualsAndHashCode.Exclude
+    @ToString.Exclude
+    private Set<Employee> employees;
+}
